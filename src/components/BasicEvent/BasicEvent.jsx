dc56fc8a5cbe601f82f16267afda2a72c86f1342
Oliverr11 <mrrsaki0403@gmail.com>
2025-04-22 18:44:43 +0700

    Event Objects

--- src/components/BasicEvent/BasicEvent.jsx
+++ src/components/BasicEvent/BasicEvent.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
 const BasicEvent = () => {
-  const onChangeHandler = () => {
-    console.log("Typing");
+  const onChangeHandler = (e) => {
+    console.log(e.target.value);
   };
-  const onClickHandler = () => {
+  const onClickHandler = (e) => {
     console.log("Clicking");
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("You're submitting " );
   };
   return (
     <>
       {" "}
       <form>
         <input onChange={onChangeHandler} type="text" />
+        <button onClick={onSubmitHandler}>Click Me</button>
       </form>
-      <button onClick={onClickHandler}>Click Me</button>
     </>
   );
 };
